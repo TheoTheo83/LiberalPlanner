825f6dea7280b7c73d640aa12deab787bfbdfb01
luis cabral <119672025+luiscbrl@users.noreply.github.com>
2023-07-19 13:17:38 +0200

    site test fonctionnel

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,35 @@
-from flask import Blueprint, render_template, request,flash
+from flask import Blueprint, render_template, request,flash, redirect,url_for
+from .models import *
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user,current_user
 
 auth = Blueprint('auth',__name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template("login.html",text="test", user="luis")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password =request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user :
+            if check_password_hash(user.password,password):
+                flash('Connection reussi!',category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else :
+                flash('mot de passe incorrecte!',category='error')
+        else : 
+            flash('utilisateur inconnu !',category='error')
+
+    return render_template("login.html",user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout<p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -20,6 +39,10 @@ def sign_up():
         prenom = request.form.get('prenom')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('cet utilisateur existe déjà! veillez choisir un autre email',category='error')
 
         if len(email) < 4 :
             flash('email doit etre plus grand que 4 caracterers',category='error')
@@ -33,7 +56,12 @@ def sign_up():
             flash ('mdp trop court min 6 caracteres', category= "error" )
 
         else :
+            new_user = User(email=email, prenom=prenom,password=generate_password_hash(password1,method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            logout_user(user, remember=True)
             flash('utilisateur creer ',category='success')
+            return redirect(url_for('views.home'))
 
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html",user=current_user)
--- website/models.py
+++ website/models.py
@@ -1,17 +1,18 @@
-from . import db 
+from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Note(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now)
-    user_id = db.Column (db.Integer, db.ForeignKey('user.id'))
 
-class User (db.Model, UserMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    email = db.Column(db.String(150), unique = True)
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     prenom = db.Column(db.String(150))
-    notes =db.relationship('Note')
-    
+    notes = db.relationship('Note')

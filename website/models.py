6e2dc47ae4e3d9e9114effde5d6271f16c150b15
luis cabral <119672025+luiscbrl@users.noreply.github.com>
2023-07-19 15:43:31 +0200

    ajout patient (enregistrement ne marche pas)

--- website/models.py
+++ website/models.py
@@ -16,3 +16,30 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     prenom = db.Column(db.String(150))
     notes = db.relationship('Note')
+
+class Parent(db.Model):
+    ID_Parent = db.Column(db.Integer, primary_key=True)
+    ID_Patients = db.Column(db.Integer, db.ForeignKey('patient.ID_Patients'))
+    Prenom = db.Column(db.String(255))
+    Nom = db.Column(db.String(255))
+
+class Pathologie(db.Model):
+    ID_Pathologie = db.Column(db.Integer, primary_key=True)
+    ID_Patients = db.Column(db.Integer, db.ForeignKey('patient.ID_Patients'))
+    Pathologie = db.Column(db.String(255))
+
+class Patient(db.Model):
+    ID_Patients = db.Column(db.Integer, primary_key=True)
+    Prenom = db.Column(db.String(255))
+    Nom = db.Column(db.String(255))
+    DateNaissance = db.Column(db.Date)
+    Age = db.Column(db.Integer)
+
+    parents = db.relationship('Parent', backref='patient')
+    pathologies = db.relationship('Pathologie', backref='patient')
+
+class rdv(db.Model):
+    id_rdv = db.Column(db.Integer,primary_key= True )
+    jourRdv = db.Column (db.Date (),nullable= False)
+    ID_Patients = db.Column(db.Integer, db.ForeignKey('patient.ID_Patients'))
+    
--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required,current_user
-from .models import Note
+from .models import Note, Patient
 from . import db 
 import json 
 
@@ -32,3 +32,22 @@ def delete_note():
             db.session.delete(note)
             db.session.commit()
     return jsonify({})
+
+@views.route('/', methods=['POST'])
+@login_required
+def add_patient():
+    nom = request.form.get('nom')
+    prenom = request.form.get('prenom')
+    date_naissance = request.form.get('date_naissance')
+    age = request.form.get('age')
+
+    if len(nom) < 1 or len(prenom) < 1 or len(date_naissance) < 1 or len(age) < 1:
+        flash('Veuillez remplir tous les champs du formulaire.', category='error')
+    else:
+        new_patient = Patient(nom=nom, prenom=prenom, DateNaissance=date_naissance, Age=age)
+        db.session.add(new_patient)
+        db.session.commit()
+        flash('Patient ajouté avec succès.', category='success')
+
+    return render_template("home.html",user=current_user)
+

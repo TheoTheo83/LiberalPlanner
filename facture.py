180a1ac5da3da2c318c35d00b528016128d453fe
BLicatesi <118910905+BLicatesi@users.noreply.github.com>
2023-07-20 09:55:08 +0200

    Changement de programme, utilisation de Latex et jinja2

--- facture.py
+++ facture.py
@@ -1,88 +1,82 @@
-from fpdf import FPDF
+from jinja2 import Environment, FileSystemLoader
+import subprocess
 
 
-class PDF_Invoice(FPDF):
-    def header(self):
-        self.set_font('Arial', 'B', 12)
-        self.cell(80)
-        self.cell(30, 10, 'Devis', 1, 0, 'C')
-        self.ln(20)
+def openTex(templatFile):
+    # Créez l'environnement de rendu avec le répertoire du script Python comme chemin pour les modèles
+    env = Environment(loader=FileSystemLoader("./"))
 
-    def footer(self):
-        self.set_y(-15)
-        self.set_font('Arial', 'I', 8)
-        self.cell(0, 10, 'Page %s' % self.page_no(), 0, 0, 'C')
+    # Chargez le modèle à partir de l'environnement
+    template = env.get_template(templateFile)
 
-pdf = PDF_Invoice()
-pdf.add_page()
+    return env, template
 
-def enteteFacture():
 
-    pdf.set_font('Arial', 'B', 10)
-    pdf.set_xy(10, 20)
-    pdf.cell(80, 10, 'MaSociete', 0, 0, 'L')
 
-    pdf.set_font('Arial', '', 8)
-    pdf.set_xy(10, 30)
-    pdf.multi_cell(80, 10, 'MonAdresse\n75000 PARIS\nR.C.S. PARIS \nB 000 000 007\nCapital : 18000 EURO', 0, 'L')
+def changeData(data, template_file):
+    #template, env = openTex()
+    env, template = openTex(templateFile)
 
-    pdf.set_font('Arial','B',10)
-    pdf.set_xy(140, 20)
-    pdf.cell(80,10,'Client: CL01',0,0,'L')
+    output = template.render(data)
+    #print(output)
+    return output
 
-    pdf.set_font('Arial', '',8)
-    pdf.set_xy(140, 30)
-    pdf.multi_cell(80, 10, 'Ste\nM. XXXX\n3ème étage\n33, rue d\'ailleurs\n75000 PARIS', 0, 'L')
+
+
+
+def showTex():
+    ...
+
+
+def saveTex(output, outputFile):
+    with open(outputFile, "w") as file:
+        file.write(output)
+
+
+def convertTexToPdf(texFile):
+
+    template_content = texFile
+
+    # Cette ligne lance la commande pdflatex convertis donc le fichier qu'on aura sélectionner
+    # A SAVOIR QUE LE FICHIER .tex DOIT ÊTRE PRÉSENT DANS LE DOSSIER OU SE TROUVE LES SCRIPTS
+    subprocess.run(["pdflatex", "-output-directory=output", "--jobname=facture", "--interaction=batchmode"], input=template_content.encode())
+
+
+if __name__ == '__main__':
+    #convertTexToPdf(templateFile)
+    #openTex(templateFile)
+
+    # template : https://www.overleaf.com/latex/templates?q=invoice https://www.overleaf.com/project/64b7c41474711532d89d6eb5
+
+    templateFile = 'facture.tex'
+
+    data = {
+    "My Name": "Benoit LICATESI",
+    "My Street": "Marseille",
+    "My Zip": "13011",
+    "my@mail.com": "benoit.licatesi@gmail.com",
+    "Fnord Prefake": "Votre nom",
+    "23. May 2009": "01. July 2023",
+    "Customer name": "Flash MCQUEEN",
+    "Customer street": "Radiator Springs",
+    "Customer ZIP": "66",
+    "Invoice no. 1": "00001",
+    "My greeting": "Bonjour,",
+    "Example Project": "Facture",
+    "Development": "Changement de pneu",
+    "1000.00": "500 €",
+    "1": "4"
+    }
+
+    # Utilisation de la fonction changeData pour remplacer les données dans le modèle LaTeX
+    output = changeData(data,templateFile)
+    #openTex(templateFile)
     
-    return pdf
 
 
+    # Enregistrement du modèle mis à jour dans un fichier .tex
+    output_file = "output/facture.tex"
+    saveTex(output, output_file)
 
-def contenueFacture():
-    pdf.ln(20)
-    pdf.cell(0, 10, 'Règlement: Chèque à réception de facture', 0, 1, 'L')
-
-    pdf.cell(0, 10, 'Échéance: 03/12/2003', 0, 1, 'L')
-
-    pdf.cell(0, 10, 'Numéro de TVA: FR888777666', 0, 1, 'L')
-
-    pdf.cell(0, 10, 'Référence: Devis ... du ....', 0, 1, 'L')
-
-    pdf.ln(20)
-
-    cols = ['REFERENCE', 'DESIGNATION', 'MONTANT H.T.', 'TVA']
-    col_width = pdf.w / 6
-
-    pdf.set_font('Arial', 'B', 8)
-    for col in cols:
-        pdf.cell(col_width, 8, col, 1, 0, 'C')
-    pdf.ln()
-
-    pdf.set_font('Arial', '', 8)
-
-    line = ['REF1',
-            'Carte Mère MSI 6378\nProcesseur AMD 1Ghz\n128Mo SDRAM, 30 Go Disque, CD-ROM']
-    for item in line:
-        pdf.cell(col_width, 30, item, 1, 0, 'L')
-    pdf.ln()
-
-    line = ['REF2', 'Câble RS232', '1', '10.00']
-    for item in line:
-        pdf.cell(col_width, 10, item, 1, 0, 'L')
-    pdf.ln()
-
-    pdf.ln(10)
-
-    pdf.cell(0, 10, 'Total HT : 660.00', 0, 1, 'R')
-
-    pdf.cell(0, 10, 'TVA (19.6%) : 128.76', 0, 1, 'R')
-
-    pdf.cell(0, 10, 'Total TTC : 788.76', 0, 1, 'R')
-
-    return pdf
-
-# Autres éléments de la facture/devis...
-if '__main__' == __name__:
-    enteteFacture()
-    contenueFacture()
-    pdf.output('output.pdf')
+    # Conversion du fichier .tex en PDF
+    convertTexToPdf(output_file)
